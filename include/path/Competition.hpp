46f1b8dd7980464bcc90cd2193d05cc63ee21503
Matthew Sun <sunmatthewsun@gmail.com>
2026-01-18 19:44:38 -0500

    ok lowk genuinely fixed all those goofy centripetal problems

--- include/path/Competition.hpp
+++ include/path/Competition.hpp
@@ -11,18 +11,15 @@ namespace Competition {
     using namespace Compiler::Tokens;
 
     inline constexpr bool FAIL_RUN = false;
-    inline constexpr float TARGET_TIME = 0.0f;
+    inline constexpr float TARGET_TIME = 5.0f;
 
     inline constexpr auto COMMANDS = std::to_array<Compiler::Command>({
         // clang-format off
 
-        moveby(0.8f * UP),
-        moveby(0.6f * LEFT),
-
-        // moveby(UP),
-        // moveby(LEFT),
-        // moveby(DOWN),
-        // moveby(RIGHT)
+        moveby(UP),
+        moveby(LEFT),
+        moveby(DOWN),
+        moveby(RIGHT)
 
         // clang-format on
     });
@@ -30,5 +27,5 @@ namespace Competition {
     inline constexpr auto PATH = Compiler::compile(COMMANDS);
     inline constexpr auto TARGET_TIMES = Compiler::getTargetTimes(COMMANDS, PATH, TARGET_TIME);
     inline constexpr auto TURN_TIMES = Compiler::TargetTime::getTurnTimes(PATH);
-    inline constexpr Vec2 DESTINATION = Compiler::getDestination(PATH)/*  / SQUARE_SIZE */;
+    inline constexpr Vec2 DESTINATION = Compiler::getDestination(PATH) / SQUARE_SIZE;
 }
